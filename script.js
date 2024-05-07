12b65e977784349e76d12294292a4bf2b8211555
MateoGonzalezLourido <128542673+MateoGonzalezLourido@users.noreply.github.com>
2024-05-07 23:54:40 +0200

    v.03

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
+const $boton_esconder_detalles = document.querySelectorAll('#bt-esconder-detalles')
+const $boton_mas_detalles = document.querySelectorAll('#bt-mas-detalles')
+const $lista_mas_detalles = document.querySelectorAll('#lista-mas-detalles')
 globalThis.addEventListener('load', () => {
-    $lista_mas_detalles.style.display = "none"
+    $lista_mas_detalles.forEach(lista => lista.style.display = "none")
 })
-
-const $boton_esconder_detalles = document.querySelector('#bt-esconder-detalles')
-const $boton_mas_detalles = document.querySelector('#bt-mas-detalles')
-const $lista_mas_detalles = document.querySelector('#lista-mas-detalles')
-
-$boton_mas_detalles.addEventListener('click', mostrar_esconder_detalles)
-$boton_esconder_detalles.addEventListener('click', mostrar_esconder_detalles)
-
+$boton_mas_detalles.forEach(item => item.addEventListener('click', mostrar_esconder_detalles))
+$boton_esconder_detalles.forEach(item => item.addEventListener('click', mostrar_esconder_detalles))
 function mostrar_esconder_detalles() {
-    if ($lista_mas_detalles.style.display == 'none') {
-        $lista_mas_detalles.style.display = "block"
-        $boton_mas_detalles.style.display = "none"
-        $boton_esconder_detalles.style.display = "block"
-    }
-    else {
-        $lista_mas_detalles.style.display = "none"
-        $boton_mas_detalles.style.display = "block"
-        $boton_esconder_detalles.style.display = "none"
-    }
+    $lista_mas_detalles.forEach(lista => {
+        if (lista.style.display == 'none') {
+            lista.style.display = "block"
+            $boton_mas_detalles.forEach(bt => bt.style.display = "none")
+            $boton_esconder_detalles.forEach(bt => bt.style.display = "block")
+        }
+        else {
+            lista.style.display = "none"
+            $boton_mas_detalles.forEach(bt => bt.style.display = "block")
+            $boton_esconder_detalles.forEach(bt => bt.style.display = "none")
+        }
+    })
 }
